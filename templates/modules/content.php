be4d1bf58da0799752de164be04355781dc46d17
Helen Varley <hvarley@gmail.com>
2018-02-23 16:54:41 +0000

    homepage layout

--- templates/modules/content.php
+++ templates/modules/content.php
@@ -1,6 +1,6 @@
 <section class="content-section container">
   <div class="row justify-content-md-center">
-    <div class="col-sm-12 col-md-9 col-lg-7">
+    <div class="col-sm-12">
       <?php echo $this_module['content']; ?>
     </div>
   </div>
