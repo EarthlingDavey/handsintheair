1e5cd8882c1377fa35ef172bca0281de42f31314
copyandcode <code@copyandcode.co.uk>
2018-02-23 17:49:46 +0000

    header
    sticky header

--- templates/header.php
+++ templates/header.php
@@ -1,25 +1,37 @@
 <header class="">
+
+  <div class="nav-buffer"></div>
   
   <div id="mobile-navigation">
-    <div class="hamburger">menu</div>
-    <div class="mobile-links">
-      <?php
-        if (has_nav_menu('top_navigation')) :
-          wp_nav_menu(['theme_location' => 'top_navigation', 'menu_class' => 'nav']);
-        endif;
+    <div class="container-fluid">
+      <div class="row top-nav">
+        <div class="site-logo-container">
+          <a class="brand" href="<?= esc_url(home_url('/')); ?>">
+            <img src="<?php bloginfo('template_directory');?>/assets/images/HITA-logo-main.png" class="site-logo">
+          </a>
+        </div>
+        <div class="hamburger">menu</div>
+      </div>
+      <div class="mobile-links">
+        <?php
+          if (has_nav_menu('top_navigation')) :
+            wp_nav_menu(['theme_location' => 'top_navigation', 'menu_class' => 'nav']);
+          endif;
 
-        get_search_form();
-      
-        if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-        endif;
+          get_search_form();
+        
+          if (has_nav_menu('primary_navigation')) :
+            wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+          endif;
 
-        if (has_nav_menu('primary_navigation_right_side')) :
-          wp_nav_menu(['theme_location' => 'primary_navigation_right_side', 'menu_class' => 'nav']);
-        endif;
-      ?>
+          if (has_nav_menu('primary_navigation_right_side')) :
+            wp_nav_menu(['theme_location' => 'primary_navigation_right_side', 'menu_class' => 'nav']);
+          endif;
+        ?>
+      </div>
     </div>
   </div>
+  
   <div id="main-navigation">
     <div class="container">
       <div class="row top-nav">
