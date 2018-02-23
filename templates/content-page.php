fe3afe77a58f1412f0526e52c14cfc242c0aa2f1
Davey Brown <daveybrown@users.noreply.github.com>
2018-02-23 11:46:21 +0000

    modules setup

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,2 +1,29 @@
-<?php the_content(); ?>
-<?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+<?php
+
+  $module_array = [
+    'cta_section'
+  ];
+  
+  $fields = get_fields();
+
+  if (isset($fields["modules"]) && $modules = $fields["modules"]) :
+    
+    // loop through the rows of data
+    foreach ( $modules as $module_number => $module  )	:
+
+      if( in_array ( $module["acf_fc_layout"] , $module_array )):	    
+        
+        $this_module = $module;
+
+        include get_template_directory() . '/templates/modules/' .$module["acf_fc_layout"] . '.php';
+
+      endif;
+      
+      $previous_module = $module["acf_fc_layout"];
+       
+    endforeach;
+    
+  endif;
+
+
+?>
---
+++ templates/modules/cta_section.php
@@ -0,0 +1,9 @@
+<section class="cta-section">
+
+  <h2 class="cta-section__heading"><?php the_field('cta_section_heading'); ?></h2>
+
+  <div class="cta-section__content wysiwyg">
+    <?php the_field('cta_section_content'); ?>
+  </div>
+
+</section>
